09008714e6630642e36e0dcda4fb1937bd983918
adamkov <adamkov@local>
2016-11-22 10:09:00 +0100

    Weather updater, proper display routine

--- esp-matrixclock/esp-matrixclock.ino
+++ esp-matrixclock/esp-matrixclock.ino
@@ -3,12 +3,11 @@
  * (c)2016 by Adam Kovesdi
  * version 2
  *
- * TODO: eliminate delays from display code 
  * TODO: improvements 
  *
  */
 
-// set these to your credentials (otherwise it will use mine - which are not included)
+// set these to your credentials (otherwise it will use mine - not included)
 
 // #define MYSSID			"changeme"		// network SSID
 // #define MYPASS			"meeto"				// WiFi PSK
@@ -32,41 +31,65 @@
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
-uint8_t hour, minute, second;
+unsigned long lastwupdate=0;		// last weather update EPOCH timestamp
+#define WEATHER_UPDATEINTERVAL		120 // in seconds
 
-void setup(){
-	initMAX7219();
-	drawString(0,font," Conn...");
-  WiFi.begin(MYSSID, MYPASS);
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-  }
-  timeClient.begin();
-	drawString(0,font,"link up");
-	timeClient.setTimeOffset(3600);
-	getWeatherData(WEATHERKEY, CITYID);
+void refresh_weather()
+{
+	timeClient.update();
+	if(timeClient.getEpochTime() > (lastwupdate+WEATHER_UPDATEINTERVAL))
+	{
+		lastwupdate=timeClient.getEpochTime();
+		getWeatherData(WEATHERKEY, CITYID);
+	}
 }
 
-void loop() {
-  timeClient.update();
-	hour=timeClient.getHours();
-	minute=timeClient.getMinutes();
-	second=timeClient.getSeconds();
-	draw_clock(hour,minute,second);
-	delay(100);
-
-  delay(4000);
-
+void display_weather()
+{
+	refresh_weather();
+	clr();
 	int t=getTemp();
-	char buf[10], *string;
+	char buf[6], *string;
 	sprintf(buf,"%dC",t);
 	drawString(0,font,buf);
 	drawChar(24,weather_icons,getWeatherIcon());
 	drawChar(18,winddir_icons,getWinddir());
 	for(int i=0;i<getWindspeed();i++) setPixel(16+i,7,true);
 	refreshAll();
+}
 
-  delay(4000);
+void dispay_clock()
+{
+	timeClient.update();
+	draw_clock(timeClient.getHours(),timeClient.getMinutes(),timeClient.getSeconds());
+}
+
+void setup()
+{
+	initMAX7219();
+	drawString(0,font," Conn...");
+	WiFi.begin(MYSSID, MYPASS);
+	while ( WiFi.status() != WL_CONNECTED ) {
+		delay ( 500 );
+	}
+	timeClient.begin();
+	drawString(0,font,"link up");
+	timeClient.setTimeOffset(3600);
+}
+
+void loop()
+{
+	timeClient.update();
+	uint8_t	timeslice=timeClient.getEpochTime() % 10;
+	if ((timeslice >4) && (timeslice <8))
+	{
+		display_weather();
+	}
+	else
+	{
+		dispay_clock();
+	}
+	delay(100);
 }
 
 
